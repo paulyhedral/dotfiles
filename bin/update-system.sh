012a67205ee4fd3ddf388211efe542fb2f8ffd20
Paul Schifferer <paul@schifferers.net>
2020-08-17 12:20:27 -0700

    Add system update script

---
+++ bin/update-system.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+dbdir="/Volumes/Dropbox/Dropbox"
+datadir="$dbdir/Homedir"
+system=$(hostname -s | tr "[:upper:]" "[:lower:]")
+
+pushd $datadir
+
+#brew upgrade
+topgrade -c --no-retry -y --disable go
+brew cask upgrade
+brew cleanup
+# brew list | fold > brew-recipes-$system.txt
+# brew cask list | fold > brew-cask-recipes-$system.txt
+brew bundle dump --global
+sudo gem update -N
+sudo gem list --local | awk '{print $1}' > gem-list-$system.txt
+mas list > mas-applications-$system.txt
+code --list-extensions > vscode-extensions-$system.txt
+code-insiders --list-extensions > vscode-insiders-extensions-$system.txt
+#cp -Rpv ~/.oh-my-zsh dot-oh-my-zsh-$system
+cp -Rpv ~/.gitconfig dot-gitconfig-$system
+cp -Rpv ~/.zshrc dot-zshrc-$system
+
+for tl in $(ls -1d /usr/local/texlive/????); do
+    $tl/bin/x86_64-darwin/tlmgr update --self
+    $tl/bin/x86_64-darwin/tlmgr update --all
+done
+
+sudo softwareupdate -d -i -a -R
+
+popd
