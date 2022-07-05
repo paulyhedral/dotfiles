0b1e61b0634244458cbcf86de9f351279c41ed26
Paul Schifferer <paul@schifferers.net>
2022-07-05 15:20:50 -0700

    Updates

--- bin/update-system.sh
+++ bin/update-system.sh
@@ -8,7 +8,7 @@ pushd $datadir
 
 #brew upgrade
 #brew unlink tfenv
-topgrade -c --no-retry -y --disable system github_cli_extensions containers
+topgrade -c --no-retry --disable system --disable github_cli_extensions --disable containers --disable yadm
 #brew cask upgrade
 brew cleanup
 # brew list | fold > brew-recipes-$system.txt
@@ -17,18 +17,19 @@ brew cleanup
 brew bundle dump --global -f
 cp ~/.Brewfile .
 yadm add .Brewfile
+yadm commit -m "Updated .Brewfile"
 #sudo gem update -N
 #sudo gem list --local | awk '{print $1}' > gem-list-$system.txt
-mas list > mas-applications-$system.txt
+mas list >mas-applications-$system.txt
 #code --list-extensions > vscode-extensions-$system.txt
-code-insiders --list-extensions > vscode-insiders-extensions-$system.txt
+code-insiders --list-extensions >vscode-insiders-extensions-$system.txt
 #cp -Rpv ~/.oh-my-zsh dot-oh-my-zsh-$system
 cp -Rpv ~/.gitconfig dot-gitconfig-$system
 cp -Rpv ~/.zshrc dot-zshrc-$system
 
 for tl in $(ls -1d /usr/local/texlive/????); do
-    $tl/bin/x86_64-darwin/tlmgr update --self
-    $tl/bin/x86_64-darwin/tlmgr update --all
+    $tl/bin/*-darwin/tlmgr update --self
+    $tl/bin/*-darwin/tlmgr update --all
 done
 
 sudo softwareupdate -d -i -a -R
